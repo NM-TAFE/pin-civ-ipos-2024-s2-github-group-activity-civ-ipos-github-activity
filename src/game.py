d83e64228af51ed87adbff9e571d37216d772162
20126861 <20126861@tafe.wa.edu.au>
2024-09-16 12:35:03 +0900

    feat: Add check_winner function

--- src/game.py
+++ src/game.py
@@ -4,9 +4,13 @@ class Game:
         self.board = [' '] * 9
         self.players = None
 
-    def check_winner():
-    # Winning combinations
-        return None
+    def check_winner(self):
+        # Winning combinations
+        winning_combinations = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
+        for a, b, c in winning_combinations:
+            if self.board[a] == self.board[b] == self.board[c] and self.board[a] != ' ':
+                return self.board[a]
+            return None
 
 
     def check_draw():
