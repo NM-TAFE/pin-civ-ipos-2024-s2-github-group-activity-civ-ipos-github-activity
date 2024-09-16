0c3e816feedd84b6dcbf2d392d6c849c853e1b77
ALEKSANDR PEREDELSKII <PEREDA@tdm.local>
2024-09-16 11:03:10 +0800

    Refactor code: move app.py from root to src folder

--- src/app.py
+++ src/app.py
@@ -2,7 +2,9 @@ from flask import Flask
 from app_logic import AppLogic
 import os
 
-app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '../templates'))
+template_folder = os.path.join(os.path.dirname(__file__), '../templates')
+static_folder = os.path.join(os.path.dirname(__file__), '../static')
+app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
 app_logic = AppLogic(app)
 print(app.url_map)
 
