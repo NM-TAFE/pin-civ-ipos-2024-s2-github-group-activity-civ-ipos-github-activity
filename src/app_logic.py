682aa47f0021d7481c6b2ea74ee2a047d66b9dac
ALEKSANDR PEREDELSKII <PEREDA@tdm.local>
2024-09-16 13:53:59 +0800

    Wins counter add

--- src/app_logic.py
+++ src/app_logic.py
@@ -13,15 +13,13 @@ class AppLogic:
     def setup_routes(self):
         @self.app.route('/')
         def index():
-            # winner = self.result_handler.check_winner()
-            winner = "X"
+            winner = self.result_handler.check_winner()
+            draw = self.result_handler.check_draw()
             x_wins = 0
             o_wins = 0
-            # draw = self.result_handler.check_draw()
-            draw = False
             return render_template('index.html', board=self.board,
                                    current_player=self.current_player, winner=winner, draw=draw,
-                                   x_wins=x_wins, o_wins=0)
+                                   x_wins=x_wins, o_wins=o_wins)
 
         @self.app.route('/play/<int:cell>')
         def play(cell):
